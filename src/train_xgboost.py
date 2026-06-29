d899ed360326cc53cc156b9de593e7d89611e57d
AkmalDev120 <akmalsofyann@gmail.com>
2026-06-29 17:43:06 +0700

    feat: initial script for xgboost modeling

---
+++ src/train_xgboost.py
@@ -0,0 +1,8 @@
+# Kerangka dasar training XGBoost untuk Mutia & Rizal
+def train_model():
+    print("Mempersiapkan fitur time-series dan training XGBoost...")
+    # Rumus Lag Features & Rolling Statistics ditaruh di sini besok
+    pass
+
+if __name__ == "__main__":
+    train_model()
