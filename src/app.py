9a051ea98b22d6ae9a6bba9b3578230c586d40be
AkmalDev120 <akmalsofyann@gmail.com>
2026-06-29 17:50:22 +0700

    feat: initial layout for streamlit dashboard

---
+++ src/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Dashboard Forecasting Saham BBRI")
+st.write("Selamat datang di dashboard forecasting saham BBRI. Silahkan bangun visualiasi grafik saham disini.")
+
+#Placeholder untuk hasil prediksi model XGBoost
+st.subheader("Hasil Prediksi Model XGBoost")
